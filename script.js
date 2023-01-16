cf5acab13b1c6d1512245cd38110ee02c6aa3e47
Javier Damiani <116226692+javierdamiani@users.noreply.github.com>
2023-01-16 12:01:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const editBtn = document.querySelector(".profile__info-edit_button");
+const popUp = document.querySelector(".popup");
+const closePopUpBtn = document.querySelector(
+  ".popup__container-form-toggle_close"
+);
+//Variables para jalar el nombre de la web
+let profileInfoName = document.querySelector(".profile__info-name");
+let profileInfoAbout = document.querySelector(".profile__info-explorer");
+
+let popupFormName = document.querySelector(
+  ".popup__container-form-inputs_name"
+);
+let popupFormAbout = document.querySelector(
+  ".popup__container-form-inputs_about"
+);
+let buttonSubmit = document.querySelector(
+  ".popup__container-form-inputs_button"
+);
+
+function openPopUp() {
+  popUp.classList.add("popup__opened");
+  console.log(profileInfoName);
+  console.log(popupFormName);
+  popupFormName.value = profileInfoName.textContent;
+  popupFormAbout.value = profileInfoAbout.textContent;
+}
+
+function closePopUp() {
+  popUp.classList.remove("popup__opened");
+  console.log("Se quitó la función");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileInfoName.textContent = popupFormName.value;
+  profileInfoAbout.textContent = popupFormAbout.value;
+  closePopUp();
+}
+
+editBtn.addEventListener("click", openPopUp);
+closePopUpBtn.addEventListener("click", closePopUp);
+buttonSubmit.addEventListener("click", handleProfileFormSubmit);
